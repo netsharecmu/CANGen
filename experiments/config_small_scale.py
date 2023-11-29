292f550ebd7de76c28d3393aef0ce0e727e88dd7
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-11-29 13:10:14 -0500

    add car-hacking datasets

--- experiments/config_small_scale.py
+++ experiments/config_small_scale.py
@@ -19,15 +19,54 @@ CANGen_BASE_FOLDER = '/ocean/projects/cis230033p/yyin4/CANGen'
 configs = Config()
 
 # DATASETS: Short descriptive name : file location
-# Input this manually
-# OpenXC
-DATASET_NAME_FILE = {}
-DATASET_NAME_FILE['openxc-nyc-downtown-east'] = os.path.join(
-    CANGen_BASE_FOLDER, 'data_selected', 'openxc', 'nyc_downtown_east.csv'
-)
-DATASET_NAME_FILE['india-new-delhi-railway-to-aiims'] = os.path.join(
-    CANGen_BASE_FOLDER, 'data_selected', 'openxc', 'india_New_Delhi_Railway_to_AIIMS.csv'
-)
-DATASET_NAME_FILE['taiwan-highwayno2-can'] = os.path.join(
-    CANGen_BASE_FOLDER, 'data_selected', 'openxc', 'taiwan_HighwayNo2_can.csv'
-)
+# Input this manually for datasets to be run
+DATASET_NAME_FILE = {
+    # OpenXC
+    'openxc-nyc-downtown-east': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'openxc', 'nyc_downtown_east.csv'
+    ),
+    'openxc-india-new-delhi-railway-to-aiims': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'openxc', 'india_New_Delhi_Railway_to_AIIMS.csv'
+    ),
+    'openxc-taiwan-highwayno2-can': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'openxc', 'taiwan_HighwayNo2_can.csv'
+    ),
+    'openxc-nyc-downtown-east-no-imputation': os.path.join(
+        CANGen_BASE_FOLDER, 'data', 'openxc', 'nyc', 'downtown-east', 'downtown-east_before_imputation.csv'
+    ),
+    'openxc-india-new-delhi-railway-to-aiims-no-imputation': os.path.join(
+        CANGen_BASE_FOLDER, 'data', 'openxc', 'india', 'New_Delhi_Railway_to_AIIMS', 'New_Delhi_Railway_to_AIIMS_before_imputation.csv'
+    ),
+    'openxc-taiwan-highwayno2-can-no-imputation': os.path.join(
+        CANGen_BASE_FOLDER, 'data', 'openxc', 'taiwan', 'HighwayNo2_can', 'HighwayNo2_can_before_imputation.csv'
+    ),
+
+    # Car-hacking
+    'car-hacking-dos-bits': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'DoS_dataset_aligned_train_bits.csv',
+    ),
+    'car-hacking-fuzzy-bits': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'Fuzzy_dataset_aligned_train_bits.csv',
+    ),
+    'car-hacking-rpm-bits': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'RPM_dataset_aligned_train_bits.csv',
+    ),
+    'car-hacking-gear-bits': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'gear_dataset_aligned_train_bits.csv',
+    ),
+    'car-hacking-dos-hex': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'DoS_dataset_aligned_train.csv',
+    ),
+    'car-hacking-fuzzy-hex': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'Fuzzy_dataset_aligned_train.csv',
+    ),
+    'car-hacking-rpm-hex': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'RPM_dataset_aligned_train.csv',
+    ),
+    'car-hacking-gear-hex': os.path.join(
+        CANGen_BASE_FOLDER, 'data_selected', 'car_hacking', 'gear_dataset_aligned_train.csv',
+    ),
+
+
+
+}
