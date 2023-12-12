533801a333ba11aa591d4532974e454853336b7f
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-12-12 16:41:57 -0500

    add tabddpm seq jobs on Azure

---
+++ slurm_jobs/run_tabddpm_seq.sh
@@ -0,0 +1,72 @@
+echo "openxc-nyc-downtown-east: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163944567 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163944567.out
+
+echo "openxc-nyc-downtown-east: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163945569 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163945569.out
+
+echo "openxc-nyc-downtown-east: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163946574 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163946574.out
+
+echo "openxc-india-new-delhi-railway-to-aiims: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163947576 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163947576.out
+
+echo "openxc-india-new-delhi-railway-to-aiims: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163948582 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163948582.out
+
+echo "openxc-india-new-delhi-railway-to-aiims: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163949582 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163949582.out
+
+echo "openxc-taiwan-highwayno2-can: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163950585 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163950585.out
+
+echo "openxc-taiwan-highwayno2-can: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163951589 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163951589.out
+
+echo "openxc-taiwan-highwayno2-can: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163952590 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163952590.out
+
+echo "car-hacking-dos-bits: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-dos-bits --model_name tabddpm --cur_time 20231212163953595 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-dos-bits-20231212163953595.out
+
+echo "car-hacking-dos-bits: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-dos-bits --model_name tabddpm --cur_time 20231212163954598 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-dos-bits-20231212163954598.out
+
+echo "car-hacking-dos-bits: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-dos-bits --model_name tabddpm --cur_time 20231212163955603 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-dos-bits-20231212163955603.out
+
+echo "car-hacking-fuzzy-bits: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-fuzzy-bits --model_name tabddpm --cur_time 20231212163956606 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-fuzzy-bits-20231212163956606.out
+
+echo "car-hacking-fuzzy-bits: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-fuzzy-bits --model_name tabddpm --cur_time 20231212163957608 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-fuzzy-bits-20231212163957608.out
+
+echo "car-hacking-fuzzy-bits: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-fuzzy-bits --model_name tabddpm --cur_time 20231212163958613 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-fuzzy-bits-20231212163958613.out
+
+echo "car-hacking-rpm-bits: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-rpm-bits --model_name tabddpm --cur_time 20231212163959614 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-rpm-bits-20231212163959614.out
+
+echo "car-hacking-rpm-bits: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-rpm-bits --model_name tabddpm --cur_time 20231212164000615 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-rpm-bits-20231212164000615.out
+
+echo "car-hacking-rpm-bits: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-rpm-bits --model_name tabddpm --cur_time 20231212164001621 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-rpm-bits-20231212164001621.out
+
+echo "car-hacking-gear-bits: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-gear-bits --model_name tabddpm --cur_time 20231212164002623 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-gear-bits-20231212164002623.out
+
+echo "car-hacking-gear-bits: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-gear-bits --model_name tabddpm --cur_time 20231212164003626 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-gear-bits-20231212164003626.out
+
+echo "car-hacking-gear-bits: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-gear-bits --model_name tabddpm --cur_time 20231212164004630 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-gear-bits-20231212164004630.out
+
+echo "syncan-flag: iteration 0"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164005631 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164005631.out
+
+echo "syncan-flag: iteration 1"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164006634 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164006634.out
+
+echo "syncan-flag: iteration 2"
+cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164007638 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164007638.out
+
