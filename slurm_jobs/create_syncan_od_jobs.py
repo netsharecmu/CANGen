375cb4ec8a2d8820b15b095d475f77993544c374
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-12-15 13:06:20 -0500

    NN in dbscan is quite memory-intensive .. bring down sample size further

--- slurm_jobs/create_syncan_od_jobs.py
+++ slurm_jobs/create_syncan_od_jobs.py
@@ -30,6 +30,12 @@ for train_file, test_file, od_model in product(synthetic_train_files, syncan_tes
     # print(train_file, test_file, od_model)
     # Create a job for each combination
     job_name = f"od,{os.path.splitext(os.path.basename(train_file))[0]},{os.path.splitext(os.path.basename(test_file))[0]},{od_model}"
+    # print(job_name)
+    results_json_file = f"../results/vehiclesec2024/small-scale/syncan_od_results/{job_name}.json"
+
+    if os.path.exists(results_json_file):
+        continue
+
     print(job_name)
     with open(os.path.join("syncan_od_jobs", job_name+".job"), 'w') as f:
         f.write(f"#!/bin/bash\n")
@@ -46,10 +52,8 @@ for train_file, test_file, od_model in product(synthetic_train_files, syncan_tes
         f.write("module load anaconda3\n")
         f.write("conda activate CANGen\n\n")
 
-        results_json_file = f"../results/vehiclesec2024/small-scale/syncan_od_results/{job_name}.json"
-
         f.write(
-            f"python3 syncan-outlier-detection-sklearn.py --train_csv_path {train_file} --test_csv_path {test_file} --results_json_file {results_json_file} --model_type {od_model} --sample_size 0.1\n")
+            f"python3 syncan-outlier-detection-sklearn.py --train_csv_path {train_file} --test_csv_path {test_file} --results_json_file {results_json_file} --model_type {od_model} --sample_size 0.01\n")
         f.close()
 
     time.sleep(1)
