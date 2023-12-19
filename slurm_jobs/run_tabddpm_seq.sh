a32b5ef780fa3bfd9b901330e0acc192c03555cd
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-12-19 17:31:49 -0500

    add more tabddpm carhacking seq jobs

--- slurm_jobs/run_tabddpm_seq.sh
+++ slurm_jobs/run_tabddpm_seq.sh
@@ -1,29 +1,29 @@
-echo "openxc-nyc-downtown-east: iteration 0"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163944567 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163944567.out
+# echo "openxc-nyc-downtown-east: iteration 0"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163944567 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163944567.out
 
-echo "openxc-nyc-downtown-east: iteration 1"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163945569 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163945569.out
+# echo "openxc-nyc-downtown-east: iteration 1"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163945569 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163945569.out
 
-echo "openxc-nyc-downtown-east: iteration 2"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163946574 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163946574.out
+# echo "openxc-nyc-downtown-east: iteration 2"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-nyc-downtown-east --model_name tabddpm --cur_time 20231212163946574 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-nyc-downtown-east-20231212163946574.out
 
-echo "openxc-india-new-delhi-railway-to-aiims: iteration 0"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163947576 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163947576.out
+# echo "openxc-india-new-delhi-railway-to-aiims: iteration 0"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163947576 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163947576.out
 
-echo "openxc-india-new-delhi-railway-to-aiims: iteration 1"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163948582 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163948582.out
+# echo "openxc-india-new-delhi-railway-to-aiims: iteration 1"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163948582 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163948582.out
 
-echo "openxc-india-new-delhi-railway-to-aiims: iteration 2"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163949582 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163949582.out
+# echo "openxc-india-new-delhi-railway-to-aiims: iteration 2"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-india-new-delhi-railway-to-aiims --model_name tabddpm --cur_time 20231212163949582 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-india-new-delhi-railway-to-aiims-20231212163949582.out
 
-echo "openxc-taiwan-highwayno2-can: iteration 0"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163950585 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163950585.out
+# echo "openxc-taiwan-highwayno2-can: iteration 0"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163950585 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163950585.out
 
-echo "openxc-taiwan-highwayno2-can: iteration 1"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163951589 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163951589.out
+# echo "openxc-taiwan-highwayno2-can: iteration 1"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163951589 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163951589.out
 
-echo "openxc-taiwan-highwayno2-can: iteration 2"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163952590 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163952590.out
+# echo "openxc-taiwan-highwayno2-can: iteration 2"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name openxc-taiwan-highwayno2-can --model_name tabddpm --cur_time 20231212163952590 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-openxc-taiwan-highwayno2-can-20231212163952590.out
 
 echo "car-hacking-dos-bits: iteration 0"
 cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-dos-bits --model_name tabddpm --cur_time 20231212163953595 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-dos-bits-20231212163953595.out
@@ -61,12 +61,12 @@ cd /storage/CANGen/experiments && python3 driver.py --config_partition small-sca
 echo "car-hacking-gear-bits: iteration 2"
 cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name car-hacking-gear-bits --model_name tabddpm --cur_time 20231212164004630 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-car-hacking-gear-bits-20231212164004630.out
 
-echo "syncan-flag: iteration 0"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164005631 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164005631.out
+# echo "syncan-flag: iteration 0"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164005631 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164005631.out
 
-echo "syncan-flag: iteration 1"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164006634 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164006634.out
+# echo "syncan-flag: iteration 1"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164006634 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164006634.out
 
-echo "syncan-flag: iteration 2"
-cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164007638 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164007638.out
+# echo "syncan-flag: iteration 2"
+# cd /storage/CANGen/experiments && python3 driver.py --config_partition small-scale --dataset_name syncan-flag --model_name tabddpm --cur_time 20231212164007638 --order_csv_by_timestamp > ../slurm_jobs/small_scale/small-scale-tabddpm-syncan-flag-20231212164007638.out
 
